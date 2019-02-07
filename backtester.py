83de8c06dfd0b5881a768c92fb9cbe02700d1762
tulski-s <tulacz@gmail.com>
2019-02-07 08:39:06 +0000

    tested whole trading period. there are still some dirty things but its getting better

--- backtester.py
+++ backtester.py
@@ -1,4 +1,5 @@
 # build-in
+import argparse
 
 # 3rd party
 import numpy as np
@@ -19,6 +20,10 @@ def get_strategy_signals(symbol):
 
     # split data into optimize/validate
     length = len(etf_full.index)
+
+    # this splitting to half is actually pretty bad. as one can have mutiple timspans
+    # for example, half of ETF woth 10y data is 5y and half of 2y history is 1y
+    # if that is used for stock universe with multiple stocks then dates will not overlapp as they should
     etf_t = etf_full.iloc[:length//2, :].copy()
     etf_v = etf_full.iloc[length//2:, :].copy()
     
@@ -116,15 +121,21 @@ class Backtester():
             days = days[:test_days]
 
         for ds in days:
-            # print('in {}, following symbols are available: {}'.format(str(ds), symbols_in_day[ds]))
-
+            log('in {}, following symbols are available: {}'.format(str(ds), symbols_in_day[ds]))
             owned_shares = list(self._owned_shares.keys())
             for symbol in owned_shares:
+
+                # safe check if missing ds for given owned symbol
+                if not symbol in symbols_in_day[ds]:
+                    continue
+                log('checking for sale symbol: {} from owned shares'.format(symbol))
+                log('_owned_shares val for given symbol: ', self._owned_shares[symbol])
+
                 if self.signals[symbol]['exit_long'][ds] == 1:
-                    # print('exit long signal for: ', symbol)
+                    log('exit long signal for: ', symbol)
                     self._sell(symbol, 'long', self.signals[symbol][self.price_label], ds)
                 elif self.signals[symbol]['exit_short'][ds] == 1:
-                    # print('exit short signal for: ', symbol)
+                    log('exit short signal for: ', symbol)
                     self._sell(symbol, 'short', self.signals[symbol][self.price_label], ds)
 
             purchease_candidates = []
@@ -134,7 +145,7 @@ class Backtester():
                 elif self.signals[sym]['entry_short'][ds] == 1:
                     purchease_candidates.append(self._define_candidate(sym, ds, 'short'))
 
-            # print('     there are following candidates to buy: ', purchease_candidates)
+            log('     there are following candidates to buy: ', purchease_candidates)
 
             symbols_to_buy = self.buying_decisions(purchease_candidates)
 
@@ -143,7 +154,7 @@ class Backtester():
 
             self._summarize_day(ds)
 
-            # print('-> NAV after session({}) is : {}'.format(ds, self._net_account_value[ds]))
+            log('-> NAV after session({}) is : {}'.format(ds, self._net_account_value[ds]))
 
         return self._run_output(), self._trades
 
@@ -161,6 +172,7 @@ class Backtester():
         self._account_value = {}
         self._net_account_value = {}
         self._rate_of_return = {}
+        self._backup_close_prices = {}
         # I'll calcualte daily returns later. its easier when I have df as I;ll be able to just shift
         # To find the return 𝑅(𝑡1,𝑡2) between dates 𝑡1 and 𝑡2 one takes 𝑅(𝑡1,𝑡2)=𝑁𝐴𝑉(𝑡2)/𝑁𝐴𝑉(𝑡1)−1
 
@@ -169,7 +181,7 @@ class Backtester():
         price = prices[ds]
         shares_count = self._owned_shares[symbol]['cnt']
         fee = self.calculate_fee(shares_count*price)
-        # print('          selling. fee is: ', fee)
+        log('          selling. fee is: ', fee)
         trx_value = (shares_count*price)
         if exit_type == 'long':
             trx_value_gross = trx_value - fee
@@ -178,7 +190,7 @@ class Backtester():
         
         self._available_money += trx_value_gross
 
-        # print('available money after sell: ', self._available_money)
+        log('available money after sell: ', self._available_money)
         
         self._trades[self._owned_shares[symbol]['trx_id']].update({
             'sell_ds': ds,
@@ -210,12 +222,12 @@ class Backtester():
             'trx_value_gross': trx_value_gross,
         }
 
-        # print('entered trade: [{}]. Details: {}. No. shares after trx: {}'.format(
-        #     trx_id,
-        #     self._trades[trx_id],
-        #     self._owned_shares[trx['symbol']],
-        #     )
-        # )
+        log('entered trade: [{}]. Details: {}. No. shares after trx: {}'.format(
+            trx_id,
+            self._trades[trx_id],
+            self._owned_shares[trx['symbol']],
+            )
+        )
 
     def _define_candidate(self, symbol, ds, entry_type):
         """Reutrns dictionary with purchease candidates and necessery keys."""
@@ -229,7 +241,20 @@ class Backtester():
         """Sets up summaries after finished session day."""
         _account_value = 0
         for symbol, vals in self._owned_shares.items():
-            _account_value += vals['cnt'] * self.signals[symbol][self.price_label][ds]
+            try:
+                price = self.signals[symbol][self.price_label][ds]
+            except KeyError:
+                # in case of missing ds in symbol take previous price value
+                # TODO(slaw): log here that there was missing date with status such that it shows even if DEBUG is off
+                price, price_ds = self._backup_close_prices[symbol]
+                print(30*' ', '!!! Using backup price from {} for {} as there was no data for it at {} !!!'.format(
+                    price_ds, symbol, ds
+                ))
+                
+            _account_value += vals['cnt'] * price
+            self._backup_close_prices[symbol] = (price, ds)
+
+
         nav = _account_value + self._available_money
         self._account_value[ds] = _account_value
         self._net_account_value[ds] = nav
@@ -258,29 +283,43 @@ def main():
     stock_1_test, stock_1_val = get_strategy_signals(sn1)
     stock_2_test, stock_2_val = get_strategy_signals(sn2)
 
-    tdays = 15
+    tdays = 100
 
     signals = {
         sn1: stock_1_test,
         sn2: stock_2_test,
     }
 
-    print(signals['ETFW20L'].head(tdays))
+    # print(signals['ETFW20L'].head(tdays))
 
     backtester = Backtester(signals)
-    results, trades = backtester.run(test_days=tdays)
 
-    print('\nFirst 5 results are: ', results.head(5))
+    # results, trades = backtester.run(test_days=tdays)
+    results, trades = backtester.run()
 
-    # print(pd.DataFrame(backtester.signals['ETFW20L']).head(5))
+    # print('\nFirst 5 results are: ', results.head(5))
+
+    # log(pd.DataFrame(backtester.signals['ETFW20L']).head(5))
     # ttt = pd.DataFrame(backtester.signals['ETFW20L']).head(5)
     # edf = pd.DataFrame()
     # xxx = pd.concat([ttt, edf], axis=1)
-    # print(xxx)
+    # log(xxx)
 
+
+def log(*args):
+    if LOG == True:
+        print(*args)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log', action='store_true')
+    args = parser.parse_args()
+    if args.log == False:
+        LOG = False
+    else:
+        LOG = True
+    
     main()
 
 
@@ -290,7 +329,8 @@ TODOs
     OK + buy
     OK + sell
     OK + summarize day
-    + logging
+    OK + test whole trading period
+    OK + "logging"
 
 - implement evaluation of the results (may be as a separate object)
 - test your previous strategy (if gives same results)
@@ -298,4 +338,6 @@ TODOs
 - test you previous strategy based on different buying_decisions settings
 
 - clean code, enhence logging, write tests
+    https://docs.python.org/3/howto/logging.html
+    https://docs.python-guide.org/writing/logging/
 """

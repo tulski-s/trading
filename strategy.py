5dfb9ab0834e3882d28af6e1214d13588cd61790
tulski-s <tulacz@gmail.com>
2019-05-25 22:09:47 +0100

    add saving itermidiate results into csv

--- strategy.py
+++ strategy.py
@@ -1,4 +1,5 @@
-# buil in
+# built-in
+import csv
 import itertools
 import traceback
 
@@ -15,7 +16,7 @@ import results
 def optimize_strategy(
         data=None, signal_gen_func=None, strategy_args=None, strategy_kwargs=None, 
         position_sizer=None, init_capital=None, optimize_for='sharpe', show_all=False,
-        logger=None, debug=None,
+        logger=None, debug=None, results_path=None
     ):
     log = commons.setup_logging(logger=logger, debug=debug)
     options = {}
@@ -30,6 +31,10 @@ def optimize_strategy(
     combinations = itertools.product(*(options[k] for k in all_names))
     # execute strategy for each comibinations
     all_metrics = []
+    if results_path:
+        fh = open(results_path, 'w')
+        writer = csv.writer(fh)
+        writer.writerow([all_names])
     for combination in combinations:
         # unpack args and kwargs for each combination
         args = [a for a in combination[:len(args_names)]]
@@ -39,14 +44,17 @@ def optimize_strategy(
             backtest = backtester.Backtester(signals, position_sizer=position_sizer, init_capital=init_capital)
             res, trades = backtest.run()
             metrics = results.evaluate(res, trades)
+            if results_path:
+                writer.writerow([combination, metrics, metrics[optimize_for]])
         except Exception:
             log.warning('Not able to run optimization for: {combination}{names}. Got following exception:\n{err}'.format(
                 combination=combination, names=all_names, err=traceback.format_exc()
             ))
             continue
-        # TODO(stulski): here also should be optional saving into the itermidiate file in case of long runs
         all_metrics.append((all_names, combination, metrics))
     all_metrics = sorted(all_metrics, key=lambda x: x[2][optimize_for], reverse=True)
+    if results_path:
+        fh.close()
     if show_all:
         return all_metrics
     return all_metrics[0]
@@ -109,6 +117,7 @@ def main():
         position_sizer=position_size.MaxFirstEncountered(),
         init_capital=10000,
         show_all=True,
+        results_path='./optimization_results.csv'
     )
     print('Optimization results: ', res)
 

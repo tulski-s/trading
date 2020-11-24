7f6c0c4bdd7c76e9802594521a0225c672a286c7
Slawomir Tulski <slaw@fb.com>
2020-11-24 17:45:18 +0000

    add getting FTSE data using investpy

---
+++ ftse_symbols.py
@@ -0,0 +1,505 @@
+# The Financial Times Stock Exchange 100 Index is a share index of the 100 companies by capital value listed on the 
+# London Stock Exchange with the highest market capitalisation.
+
+ftse_100 = [
+  {
+    "name": "3i",
+    "symbol": "III",
+    "sector": "Financial Services"
+  },
+  {
+    "name": "Admiral Group",
+    "symbol": "ADML",
+    "sector": "Nonlife Insurance"
+  },
+  {
+    "name": "Anglo American plc",
+    "symbol": "AAL",
+    "sector": "Mining"
+  },
+  {
+    "name": "Antofagasta",
+    "symbol": "ANTO",
+    "sector": "Mining"
+  },
+  {
+    "name": "Ashtead Group",
+    "symbol": "AHT",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Associated British Foods",
+    "symbol": "ABF",
+    "sector": "Food Producers"
+  },
+  {
+    "name": "AstraZeneca",
+    "symbol": "AZN",
+    "sector": "Pharmaceuticals & Biotechnology"
+  },
+  {
+    "name": "Auto Trader Group",
+    "symbol": "AUTOA",
+    "sector": "Media"
+  },
+  # {
+  #   "name": "Avast",
+  #   "symbol": "AVST",
+  #   "sector": "Software and Computer Services"
+  # },
+  {
+    "name": "Aveva",
+    "symbol": "AVV",
+    "sector": "Software & Computer Services"
+  },
+  {
+    "name": "Aviva",
+    "symbol": "AV",
+    "sector": "Life Insurance"
+  },
+  {
+    "name": "B&M",
+    "symbol": "BMEB",
+    "sector": "Retailers"
+  },
+  {
+    "name": "BAE Systems",
+    "symbol": "BAES",
+    "sector": "Aerospace & Defence"
+  },
+  {
+    "name": "Barclays",
+    "symbol": "BARC",
+    "sector": "Banks"
+  },
+  {
+    "name": "Barratt Developments",
+    "symbol": "BDEV",
+    "sector": "Household Goods & Home Construction"
+  },
+  {
+    "name": "Berkeley Group Holdings",
+    "symbol": "BKGH",
+    "sector": "Household Goods & Home Construction"
+  },
+  {
+    "name": "BHP",
+    "symbol": "BHPB",
+    "sector": "Mining"
+  },
+  {
+    "name": "BP",
+    "symbol": "BP",
+    "sector": "Oil & Gas Producers"
+  },
+  {
+    "name": "British American Tobacco",
+    "symbol": "BATS",
+    "sector": "Tobacco"
+  },
+  {
+    "name": "British Land",
+    "symbol": "BLND",
+    "sector": "Real Estate Investment Trusts"
+  },
+  {
+    "name": "BT Group",
+    "symbol": "BT",
+    "sector": "Fixed Line Telecommunications"
+  },
+  {
+    "name": "Bunzl",
+    "symbol": "BNZL",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Burberry",
+    "symbol": "BRBY",
+    "sector": "Personal Goods"
+  },
+  {
+    "name": "Coca-Cola HBC",
+    "symbol": "CCH",
+    "sector": "Beverages"
+  },
+  {
+    "name": "Compass Group",
+    "symbol": "CPG",
+    "sector": "Support Services"
+  },
+  {
+    "name": "CRH plc",
+    "symbol": "CRH",
+    "sector": "Construction & Materials"
+  },
+  {
+    "name": "Croda International",
+    "symbol": "CRDA",
+    "sector": "Chemicals"
+  },
+  {
+    "name": "DCC plc",
+    "symbol": "DCC",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Diageo",
+    "symbol": "DGE",
+    "sector": "Beverages"
+  },
+  {
+    "name": "Evraz",
+    "symbol": "EVRE",
+    "sector": "Industrial Metals & Mining"
+  },
+  {
+    "name": "Experian",
+    "symbol": "EXPN",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Ferguson plc",
+    "symbol": "FERG",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Flutter Entertainment",
+    "symbol": "FLTRF",
+    "sector": "Travel & Leisure"
+  },
+  {
+    "name": "Fresnillo",
+    "symbol": "FRES",
+    "sector": "Mining"
+  },
+  {
+    "name": "GlaxoSmithKline",
+    "symbol": "GSK",
+    "sector": "Pharmaceuticals & Biotechnology"
+  },
+  {
+    "name": "Glencore",
+    "symbol": "GLEN",
+    "sector": "Mining"
+  },
+  {
+    "name": "GVC Holdings",
+    "symbol": "GVC",
+    "sector": "Travel and Leisure"
+  },
+  {
+    "name": "Halma",
+    "symbol": "HLMA",
+    "sector": "Electronic & Electrical Equipment"
+  },
+  {
+    "name": "Hargreaves Lansdown",
+    "symbol": "HRGV",
+    "sector": "Financial Services"
+  },
+  {
+    "name": "Hikma Pharmaceuticals",
+    "symbol": "HIK",
+    "sector": "Pharmaceuticals & Biotechnology"
+  },
+  {
+    "name": "Homeserve",
+    "symbol": "HSV",
+    "sector": "Consumer services"
+  },
+  {
+    "name": "HSBC",
+    "symbol": "HSBA",
+    "sector": "Banks"
+  },
+  {
+    "name": "Imperial Brands",
+    "symbol": "IMB",
+    "sector": "Tobacco"
+  },
+  {
+    "name": "Informa",
+    "symbol": "INF",
+    "sector": "Media"
+  },
+  {
+    "name": "InterContinental Hotels Group",
+    "symbol": "IHG",
+    "sector": "Travel & Leisure"
+  },
+  {
+    "name": "Intermediate Capital Group",
+    "symbol": "ICP",
+    "sector": "Investment Services"
+  },
+  {
+    "name": "International Consolidated Airlines Group",
+    "symbol": "ICAG",
+    "sector": "Travel & Leisure"
+  },
+  {
+    "name": "Intertek",
+    "symbol": "ITRK",
+    "sector": "Support Services"
+  },
+  {
+    "name": "JD Sports",
+    "symbol": "JD",
+    "sector": "General Retailers"
+  },
+  {
+    "name": "Johnson Matthey",
+    "symbol": "JMAT",
+    "sector": "Chemicals"
+  },
+  {
+    "name": "Just Eat Takeaway",
+    "symbol": "JE",
+    "sector": "Software and Computer Services"
+  },
+  {
+    "name": "Kingfisher",
+    "symbol": "KGF",
+    "sector": "Retailers"
+  },
+  {
+    "name": "Land Securities",
+    "symbol": "LAND",
+    "sector": "Real Estate Investment Trusts"
+  },
+  {
+    "name": "Legal & General",
+    "symbol": "LGEN",
+    "sector": "Life Insurance"
+  },
+  {
+    "name": "Lloyds Banking Group",
+    "symbol": "LLOY",
+    "sector": "Banks"
+  },
+  {
+    "name": "London Stock Exchange Group",
+    "symbol": "LSE",
+    "sector": "Financial Services"
+  },
+  # {
+  #   "name": "M&G",
+  #   "symbol": "MNG",
+  #   "sector": "Asset Managers"
+  # },
+  {
+    "name": "Melrose Industries",
+    "symbol": "MRON",
+    "sector": "Automobiles & Parts"
+  },
+  {
+    "name": "Mondi",
+    "symbol": "MNDI",
+    "sector": "Forestry & Paper"
+  },
+  {
+    "name": "Morrisons",
+    "symbol": "MRW",
+    "sector": "Food & Drug Retailers"
+  },
+  {
+    "name": "National Grid plc",
+    "symbol": "NG",
+    "sector": "Gas, Water & Multi-utilities"
+  },
+  {
+    "name": "Royal Bank of Scotland Group PLC",
+    "symbol": "RBS",
+    "sector": "Banks"
+  },
+  {
+    "name": "Next plc",
+    "symbol": "NXT",
+    "sector": "General Retailers"
+  },
+  {
+    "name": "Ocado",
+    "symbol": "OCDO",
+    "sector": "Food & Drug Retailers"
+  },
+  {
+    "name": "Pearson plc",
+    "symbol": "PSON",
+    "sector": "Media"
+  },
+  {
+    "name": "Pennon Group",
+    "symbol": "PNN",
+    "sector": "Water"
+  },
+  {
+    "name": "Persimmon plc",
+    "symbol": "PSN",
+    "sector": "Household Goods & Home Construction"
+  },
+  {
+    "name": "Phoenix Group",
+    "symbol": "PHNX",
+    "sector": "Life Insurance"
+  },
+  {
+    "name": "Polymetal International",
+    "symbol": "POLYP",
+    "sector": "Precious Metals and Mining"
+  },
+  {
+    "name": "Prudential plc",
+    "symbol": "PRU",
+    "sector": "Life Insurance"
+  },
+  {
+    "name": "Reckitt Benckiser",
+    "symbol": "RB",
+    "sector": "Household Goods & Home Construction"
+  },
+  {
+    "name": "RELX",
+    "symbol": "REL",
+    "sector": "Media"
+  },
+  {
+    "name": "Rentokil Initial",
+    "symbol": "RTO",
+    "sector": "Support Services"
+  },
+  {
+    "name": "Rio Tinto",
+    "symbol": "RIO",
+    "sector": "Mining"
+  },
+  {
+    "name": "Rightmove",
+    "symbol": "RMV",
+    "sector": "Media"
+  },
+  {
+    "name": "Rolls-Royce Holdings",
+    "symbol": "RR",
+    "sector": "Aerospace & Defence"
+  },
+  {
+    "name": "Royal Dutch Shell",
+    "symbol": "RDSa",
+    "sector": "Oil & Gas Producers"
+  },
+  {
+    "name": "RSA Insurance Group",
+    "symbol": "RSA",
+    "sector": "Nonlife Insurance"
+  },
+  {
+    "name": "Sage Group",
+    "symbol": "SGE",
+    "sector": "Software & Computer Services"
+  },
+  {
+    "name": "Sainsbury's",
+    "symbol": "SBRY",
+    "sector": "Food & Drug Retailers"
+  },
+  {
+    "name": "Schroders",
+    "symbol": "SDR",
+    "sector": "Financial Services"
+  },
+  {
+    "name": "Scottish Mortgage Investment Trust",
+    "symbol": "SMT",
+    "sector": "Equity Investment Instruments"
+  },
+  {
+    "name": "Segro",
+    "symbol": "SGRO",
+    "sector": "Real Estate Investment Trusts"
+  },
+  {
+    "name": "Severn Trent",
+    "symbol": "SVT",
+    "sector": "Gas, Water & Multi-utilities"
+  },
+  {
+    "name": "Smith & Nephew",
+    "symbol": "SN",
+    "sector": "Health Care Equipment & Services"
+  },
+  {
+    "name": "DS Smith",
+    "symbol": "SMDS",
+    "sector": "General Industrials"
+  },
+  {
+    "name": "Smiths Group",
+    "symbol": "SMIN",
+    "sector": "General Industrials"
+  },
+  {
+    "name": "Smurfit Kappa",
+    "symbol": "SKG",
+    "sector": "General Industrials"
+  },
+  {
+    "name": "Spirax-Sarco Engineering",
+    "symbol": "SPX",
+    "sector": "Industrial Engineering"
+  },
+  {
+    "name": "SSE plc",
+    "symbol": "SSE",
+    "sector": "Electricity"
+  },
+  {
+    "name": "Standard Chartered",
+    "symbol": "STAN",
+    "sector": "Banks"
+  },
+  {
+    "name": "Standard Life Aberdeen",
+    "symbol": "SLA",
+    "sector": "Financial Services"
+  },
+  {
+    "name": "St. James's Place plc",
+    "symbol": "SJP",
+    "sector": "Life Insurance"
+  },
+  {
+    "name": "Taylor Wimpey",
+    "symbol": "TW",
+    "sector": "Household Goods & Home Construction"
+  },
+  {
+    "name": "Tesco",
+    "symbol": "TSCO",
+    "sector": "Food & Drug Retailers"
+  },
+  {
+    "name": "Unilever",
+    "symbol": "ULVR",
+    "sector": "Personal Goods"
+  },
+  {
+    "name": "United Utilities",
+    "symbol": "UU",
+    "sector": "Gas, Water & Multi-utilities"
+  },
+  {
+    "name": "Vodafone Group",
+    "symbol": "VOD",
+    "sector": "Mobile Telecommunications"
+  },
+  {
+    "name": "Whitbread",
+    "symbol": "WTB",
+    "sector": "Retail hospitality"
+  },
+  {
+    "name": "WPP plc",
+    "symbol": "WPP",
+    "sector": "Media"
+  }
+]
---
+++ lse_data.py
@@ -0,0 +1,123 @@
+# built in
+import csv
+import datetime
+import os
+import time
+
+# 3rd party
+import investpy
+import pandas as pd
+import numpy as np
+
+
+# custom
+from ftse_symbols import (
+    ftse_100,
+) 
+
+
+class LSEData():
+    def __init__(self, pricing_data_path='./pricing_data'):
+        self.country = 'united kingdom'
+        self.indicies_stocks = {
+            'FTSE100': [el['symbol'] for el in ftse_100]
+        }
+        self.pricing_data_path = pricing_data_path
+        self.column_names = ['date', 'open', 'high', 'low', 'close', 'volume']
+
+    def load(self, symbols=None, df=True, from_csv=True):
+        """
+        Returns pricing data for *symbols*. *symbols* may be single symbol as a string or
+        iterable with symbols. 
+
+        If *from_csv* is True it reads data from csv, else it gets it from the web. Currently it takes
+        full history until execution.
+
+        Output is pandas DataFrame or list of lists with ['date','open','high','low','close','volume']
+        if only 1 symbol is provided. If more symbols requested then the output will be a dictionary with 
+        symbol as a key and data with the same format as the one for single symbol.
+        """
+        if isinstance(symbols, str):
+            symbols = [symbols]
+        pricing_data = {}
+        for symbol in symbols:
+            if from_csv:
+                data_df = pd.read_csv(self._output_path(symbol))
+                date_col = self.column_names[0]
+                data_df.set_index(pd.DatetimeIndex(data_df[date_col]), inplace=True)
+                data_df.drop(date_col, axis=1, inplace=True)
+            else:
+                data_df = self._get_stock_historical_data(symbol)
+                data_df.drop('Currency', axis=1, inplace=True)
+                data_df.rename(columns={c: c.lower() for c in data_df.columns}, inplace=True)
+                data_df.index.rename('date', inplace=True)
+            data_df.name=symbol
+            data_df.replace(to_replace=0, value=np.nan, inplace=True)
+            data_df.fillna(method='ffill', inplace=True)
+            if df == True:
+                pricing_data[symbol] = data_df
+            else:
+                pricing_data[symbol] = [
+                    [datetime.datetime.fromtimestamp(d.item()//1000000000).strftime('%Y-%m-%d')] + val
+                    for d,val in list(zip(data_df.index.values, data_df.values.tolist()))
+                ]
+        if len(symbols) == 1:
+            return pricing_data[symbol]
+        else:
+            return pricing_data
+
+    def download_data_to_csv(self, symbols=None):
+        """
+        Collects historical data and outputs csv file in *pricing_data_path*. Currently it takes
+        history from 1990 until execution date and its overwriting files.
+        """
+        for symbol in symbols:
+            print('Downloading {}'.format(symbol))
+            try:
+                pricing_data = self._get_stock_historical_data(symbol)
+            except ConnectionError:
+                print('Some connection issue. Waiting 60s and retrying')
+                time.sleep(60)
+                pricing_data = self._get_stock_historical_data(symbol)
+            dates = [d.date().strftime('%Y-%m-%d') for d in pricing_data.index.to_list()]
+            data_arrs = []
+            for col in self.column_names[1:]:
+                data_arrs.append(pricing_data[col.capitalize()].to_list())
+            data = list(zip(*data_arrs))
+            with open(self._output_path(symbol), 'w') as fh:
+                writer = csv.writer(fh)
+                writer.writerow(self.column_names)
+                for idx, d in enumerate(dates):
+                    writer.writerow((d,) + data[idx])
+            time.sleep(1)
+
+    def get_all_available_symbols(self):
+        """
+        Return list of all available stocks for UK in investpy
+        """
+        stocks_df = investpy.get_stocks(country=self.country)
+        return stocks_df['symbol'].to_list()
+
+    def _output_path(self, symbol):
+        return os.path.join(self.pricing_data_path, '{}_pricing.csv'.format(symbol))
+
+    def _get_stock_historical_data(self, symbol):
+        return investpy.get_stock_historical_data(
+            stock=symbol,
+            country=self.country,
+            from_date='01/01/1990',
+            to_date=datetime.date.today().strftime('%d/%m/%Y'),
+        )
+
+
+def test():
+    lse = LSEData()
+    data = lse.load(
+        symbols=['WTB', 'IHG'],
+        from_csv=True,
+    )
+
+
+if __name__ == '__main__':
+    test()
+
